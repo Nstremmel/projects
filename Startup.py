4f7ff036ce18a519834639922cf99adac284c809
nstremmel <noahstremmel22@gmail.com>
2017-12-28 18:16:10 -0600

    save

--- Startup.py
+++ Startup.py
@@ -1,5 +1,20 @@
 import os
 import sys
-#os.system("start www.ecosia.com")
-#os.system("start www.youtube.com")
-os.system("start Spotify.exe")
+
+choice=str(input(""))
+
+os.system("start www.ecosia.com")
+os.system("start www.youtube.com")
+
+if choice=="1":
+	os.system("C:\\Users\\nstremmel\\Desktop\\OSBuddy64")
+elif choice=="2":
+	os.system("C:\\Users\\nstremmel\\Desktop\\RuneScape Launcher")
+elif choice=="3":
+	os.system("C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Sublime Text 3")
+elif choice=="hourglass":
+	os.system("C:\\Users\\nstremmel\\Git\\projects\\math\\Turtle\\Hourglass.py")
+
+
+
+
